7d1029d58366f7db93655d4a00a0ea3c4274aaff
Alex Wainshtein <w.alex2k@gmail.com>
2018-01-17 16:40:04 +0200

    Fixed some problems

---
+++ WorkerManager/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkerManager.Models;
+
+namespace WorkerManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+
+        public void AdminRegister(string IdentityCard, string Name, string Password)
+        {
+            using (var db = new AdminContext())
+            {
+                //db.Database.Delete();
+                db.Admins.Add(new Admin { IdentityCard = IdentityCard, Name = Name, Password = Password });
+                db.SaveChanges();
+            }
+        }
+
+
+
+
+        public bool AdminLogin(string IdentityCard, string Password)
+        {
+            using (var db = new AdminContext())
+            {
+                foreach (var item in db.Admins)
+                {
+                    if (item.IdentityCard == IdentityCard && item.Password == Password)
+                    {
+                        return true;
+                    }
+
+                }
+            }
+            return false;
+        }
+        
+    }
+}
---
+++ WorkerManager/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WorkerManager.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string IdentityCard { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+    class AdminContext: DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
